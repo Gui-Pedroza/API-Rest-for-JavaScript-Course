d4330b2756bff8fe48c32260c595d5daf68ce7f0
Gui-Pedroza <gui_pedroza@hotmail.com>
2024-02-24 12:16:46 -0300

    JWT for Users

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import express from 'express'
 import homeRoutes from './src/routes/homeRoutes'
 import alunoRoutes from './src/routes/alunoRoutes'
 import userRoutes from './src/routes/userRoutes'
+import tokenRoutes from './src/routes/tokenRoutes'
 import './src/database/index'
 
 dotenv.config()
@@ -16,5 +17,6 @@ app.use(express.json())
 app.use('/', homeRoutes)
 app.use('/alunos', alunoRoutes)
 app.use('/users', userRoutes)
+app.use('/tokens', tokenRoutes)
 
 export default app
